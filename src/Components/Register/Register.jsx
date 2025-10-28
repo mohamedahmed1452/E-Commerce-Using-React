daa2686d6bc4e6819fc13deec6cd4d08eeeb9eaa
mohamedahmed1452 <20201700656@cis.asu.edu.eg>
2025-10-28 02:42:57 +0200

    finish

--- src/Components/Register/Register.jsx
+++ src/Components/Register/Register.jsx
@@ -80,7 +80,7 @@ export default function Register() {
             role="alert"
           >
             <span className="font-medium">Danger alert!</span> {errorMessage}
-          </div>
+          </div> 
         ) : successMessage ? (
           <div
             className="p-4 mb-4 text-sm text-green-800 rounded-lg bg-green-50 mx-auto mt-6 dark:bg-gray-800 dark:text-green-400 w-[35%] "
